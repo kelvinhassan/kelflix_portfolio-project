8bdda16ddef49c3c82930d8de1b981adba91b3ca
kelvinhassan <ngerajustin@gmail.com>
2023-04-22 03:21:07 +0300

    login logout comlete cycle

--- kelflix/src/App.js
+++ kelflix/src/App.js
@@ -3,22 +3,33 @@ import "./App.css"
 import HomeScreen from "./screens/HomeScreen"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import LoginScreen from "./screens/LoginScreen"
+import ProfileScreen from "./screens/ProfileScreen"
 import { auth } from "./firebase"
+import { useDispatch, useSelector } from "react-redux"
+import { login, logout, selectUser } from "./features/UserSlice"
 
 function App() {
-  const user = null
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
         //logged in
         console.log(userAuth)
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        )
       } else {
         //logged out
+        dispatch(logout())
       }
     })
     return unsubscribe
-  }, [])
+  }, [dispatch])
   return (
     <div className="app">
       {!user ? (
@@ -26,6 +37,9 @@ function App() {
       ) : (
         <Router>
           <Switch>
+            <Route path="/profile">
+              <ProfileScreen />
+            </Route>
             <Route path="/">
               <HomeScreen />
             </Route>
--- kelflix/src/Nav.js
+++ kelflix/src/Nav.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react"
 import "./Nav.css"
 import knlogo from "./knlogo.png"
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
 
 function Nav() {
   const [show, handleShow] = useState(false)
+  const history = useHistory()
 
   const transitionNavBar = () => {
     if (window.scrollY > 100) {
@@ -20,8 +22,14 @@ function Nav() {
   return (
     <div className={`nav ${show && "nav__black"}`}>
       <div className="nav__contennts">
-        <img className="nav__logo" src={knlogo} alt="knlogo" />
         <img
+          onClick={() => history.push("/")}
+          className="nav__logo"
+          src={knlogo}
+          alt="knlogo"
+        />
+        <img
+          onClick={() => history.push("/profile")}
           className="nav__avatar"
           src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
           alt="kelflix Logo"
